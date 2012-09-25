162fab7f282d9d5130c3f4f4c4176a90cf48a562
Michael Gruber <aelgru@gmail.com>
2012-09-25 11:41:36 +0200

    services.py

---
+++ src/main/python/pypiproxy/services.py
@@ -0,0 +1,19 @@
+__author__ = "Michael Gruber"
+
+def list_available_package_names():
+    """
+        @return: iterable of strings
+    """
+    pass
+
+def list_versions(name):
+    """
+        @return: iterable of strings
+    """
+    pass
+
+def get_package_content(name, version):
+    """
+        @return: a file-like object
+    """
+    pass
