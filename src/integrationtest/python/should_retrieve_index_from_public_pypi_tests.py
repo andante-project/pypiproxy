224c3f6e3712757e5307b14a9cd71cd6dbb6d3f5
Michael Gruber <aelgru@gmail.com>
2012-10-17 18:27:11 +0200

    simple refactoring

--- src/integrationtest/python/should_retrieve_index_from_public_pypi_tests.py
+++ src/integrationtest/python/should_retrieve_index_from_public_pypi_tests.py
@@ -3,14 +3,13 @@ from pyfix import after, before, given, test, run_tests
 from liveserver import LiveServer
 from staticpypi import StaticPyPiServer
 from integrationtestsupport import download
-from pyassert import Matcher, register_matcher
 
 @test
 def integration_test():
     with StaticPyPiServer(), LiveServer() as liveserver:
-            index_page = download(liveserver.url + "simple/")
+        index_page = download(liveserver.url + "simple/")
 
-            assert_that(index_page).contains("public-a").contains("public-b").contains("public-c")
+        assert_that(index_page).contains("public-a").contains("public-b").contains("public-c")
 
 
 if __name__=='__main__':
