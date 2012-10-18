d43773041ddc06af38a4a1e40b2b274d727a566e
Michael Gruber <aelgru@gmail.com>
2012-10-18 15:29:11 +0200

    docstrings in services

--- src/main/python/pypiproxy/services.py
+++ src/main/python/pypiproxy/services.py
@@ -33,11 +33,16 @@ def initialize_services(hosted_packages_directory, cached_packages_directory, py
     _proxy_packages_index = ProxyPackageIndex("cached", cached_packages_directory, pypi_url)
 
 def add_package(name, version, content_stream):
+    """
+        Adds a new package to the hosted package index.
+        The package is described by name, version and content.
+    """
     LOGGER.debug("Adding package '%s %s'", name, version)
     _hosted_packages_index.add_package(name, version, content_stream)
 
 def get_package_content(name, version):
     """
+        Retrieves the package identified by name and version.
         @return: a file-like object
     """
     LOGGER.debug("Retrieving package content for '%s %s'", name, version)
@@ -51,6 +56,7 @@ def get_package_content(name, version):
 
 def get_package_statistics():
     """
+        Used by the index page.
         @return: a tuple containing several statistics of the index:
             # of package files, # of unique package names
     """
@@ -61,7 +67,7 @@ def get_package_statistics():
 
 def list_available_package_names():
     """
-        @return: iterable of strings
+        @return: sorted list of strings
     """
     LOGGER.debug("Listing available packages")
     cached_packages = _proxy_packages_index.list_available_package_names()
@@ -71,6 +77,7 @@ def list_available_package_names():
 
 def list_versions(name):
     """
+        Returns the available versions for the given package name.
         @return: iterable of strings
     """
     LOGGER.debug("Listing versions for package '%s'", name)
