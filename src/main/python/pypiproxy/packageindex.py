0729438a6885f293d56d4ea78e09bf3bbb234574
Michael Gruber <aelgru@gmail.com>
2012-11-23 18:56:03 +0100

    logging download result

--- src/main/python/pypiproxy/packageindex.py
+++ src/main/python/pypiproxy/packageindex.py
@@ -134,6 +134,7 @@ class ProxyPackageIndex(object):
         versions_url = "{0}/simple/{1}/".format(self._pypi_url, name)
         LOGGER.info("Downloading versions from {0}".format(versions_url))
         versions_content = self._fetch_url(versions_url)
+        LOGGER.info("Downloaded versions page for {0} from {1} is {2} bytes.".format(name, versions_url, len(versions_content)))
 
         if versions_content is not None:
             return self._extract_versions(versions_content)
